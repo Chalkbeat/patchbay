f49203f833f2ea1b7af0881645dfe1877866b0d8
Thomas Wilburn <twilburn@chalkbeat.org>
2022-10-24 14:29:48 -0500

    Fix logo, add click-to-copy to the code box

--- src/js/demo.js
+++ src/js/demo.js
@@ -7,3 +7,9 @@ var code = codeElement.innerHTML;
 input.addEventListener("input", function() {
   codeElement.innerHTML = code.replace("EMBED_URL_HERE", input.value || "EMBED_URL_HERE");
 });
+
+codeElement.addEventListener("click", function(e) {
+  var text = codeElement.innerHTML.trim().replace(/&lt;/gi, "<").replace(/&gt;/gi, ">");
+  console.log(text);
+  navigator.clipboard.writeText(text);
+})
